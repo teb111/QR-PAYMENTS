e2df2ddc271dd763ed96b4a34b084c34d5ffb99f
teb111 <fakoredetobiloba.ft@gmail.com>
2023-04-15 09:09:58 +0100

    m2

--- frontend/src/actions/paymentAction.js
+++ frontend/src/actions/paymentAction.js
@@ -1,32 +1,30 @@
-import axios from "axios";
-import { PAYMENT_CONSTANTS } from "../services/constants";
+import axios from 'axios'
+import { PAYMENT_CONSTANTS } from '../services/constants'
 
 export const fetchTransactionAction = (id) => async (dispatch) => {
   try {
     dispatch({
-      type: PAYMENT_CONSTANTS.FETCH_TRANSACTION_REQUEST,
-    });
+      type: PAYMENT_CONSTANTS.FETCH_TRANSACTION_REQUEST
+    })
 
-    const ref_code = id;
+    const ref_code = id
     const config = {
       headers: {
-        HTTP_AUTHORIZATION_TOKEN: process.env.REACT_APP_TOKEN,
-      },
-    };
+        HTTP_AUTHORIZATION_TOKEN: process.env.REACT_APP_TOKEN
+      }
+    }
 
-
-    const { data } = await axios.post(`/fetch_payment`, { ref_code }, config);
+    const { data } = await axios.post(`/fetch_payment`, { ref_code }, config)
     dispatch({
       type: PAYMENT_CONSTANTS.FETCH_TRANSACTION_SUCCESS,
-      payload: data?.data,
-    });
-
+      payload: data?.data
+    })
   } catch (error) {
+    console.log(error)
     dispatch({
       type: PAYMENT_CONSTANTS.FETCH_TRANSACTION_FAIL,
       payload: error
     })
-
   }
 }
 
@@ -34,64 +32,64 @@ export const initialisePaymentAction = (data) => async (dispatch) => {
   const paymentData = {
     ref_code: data?.ref_code,
     amount: data?.amount,
-    phone_number: "081765783837",
+    phone_number: '081765783837',
     fullname: data?.fullname,
-    email: data?.email,
-  };
+    email: data?.email
+  }
   try {
     dispatch({
-      type: PAYMENT_CONSTANTS.INITIALISE_PAYMENT_REQUEST,
-    });
+      type: PAYMENT_CONSTANTS.INITIALISE_PAYMENT_REQUEST
+    })
 
     const config = {
       headers: {
-        HTTP_AUTHORIZATION_TOKEN: process.env.REACT_APP_TOKEN,
-      },
-    };
+        HTTP_AUTHORIZATION_TOKEN: process.env.REACT_APP_TOKEN
+      }
+    }
 
     const { data } = await axios.post(
       `/initialise_payment`,
       paymentData,
       config
-    );
+    )
     dispatch({
       type: PAYMENT_CONSTANTS.INITIALISE_PAYMENT_SUCCESS,
-      payload: data?.data,
-    });
+      payload: data?.data
+    })
   } catch (error) {
     dispatch({
       type: PAYMENT_CONSTANTS.INITIALISE_PAYMENT_FAIL,
-      payload: error,
-    });
+      payload: error
+    })
   }
-};
+}
 
+export const verifyTransactionAction =
+  (transaction_ref) => async (dispatch) => {
+    try {
+      dispatch({
+        type: PAYMENT_CONSTANTS.VERIFY_TRANSACTION_REQUEST
+      })
 
+      const config = {
+        headers: {
+          HTTP_AUTHORIZATION_TOKEN: process.env.REACT_APP_TOKEN
+        }
+      }
 
-export const verifyTransactionAction = (transaction_ref) => async (dispatch) => {
-  try {
-    dispatch({
-      type: PAYMENT_CONSTANTS.VERIFY_TRANSACTION_REQUEST,
-    });
-
-    const config = {
-      headers: {
-        HTTP_AUTHORIZATION_TOKEN: process.env.REACT_APP_TOKEN,
-      },
+      const { data } = await axios.post(
+        `/verify_payment`,
+        { transaction_ref },
+        config
+      )
+      dispatch({
+        type: PAYMENT_CONSTANTS.VERIFY_TRANSACTION_SUCCESS,
+        payload: data?.data
+      })
+    } catch (error) {
+      dispatch({
+        type: PAYMENT_CONSTANTS.VERIFY_TRANSACTION_FAIL,
+        payload: error
+      })
     }
-
-
-    const { data } = await axios.post(`/verify_payment`, { transaction_ref }, config);
-    dispatch({
-      type: PAYMENT_CONSTANTS.VERIFY_TRANSACTION_SUCCESS,
-      payload: data?.data,
-    });
-
-
-  } catch (error) {
-    dispatch({
-      type: PAYMENT_CONSTANTS.VERIFY_TRANSACTION_FAIL,
-      payload: error,
-    });
   }
-};
